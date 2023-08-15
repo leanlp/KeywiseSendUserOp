363a6c854eaeeb649b4a032a6d18e8a577e2641d
leanlp <leanlp@gmail.com>
2023-08-15 17:55:08 -0300

    stakingVault

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 KeywiseSendUserOp
 in this case of use, we modifier a SDK of ALchemy, change ABI of SimpleAccount to SmarWallet
+
+
+
+usdt mumbai=0xef9ccA0D749A362AAaEbaaC1e7434D861153F51d
+
+staking sinthetix=0xf53eefD2f5E0B8235D8B0b9FAE4eFA74c98786f0
---
+++ scripts/encodeApprove.cjs
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const ethers = require("ethers");
+const abi = require('./ABIERC20.json');
+const abiUniV2 = require('./ABIUniSwapRouterV2.json');
+
+
+const iface = new ethers.Interface(abi);
+
+
+const arg1 = "0xf53eefD2f5E0B8235D8B0b9FAE4eFA74c98786f0";  // Target Staking Contract
+const arg2 = "100000"; // second argument
+
+const encodedABI = iface.encodeFunctionData("approve", [arg1, arg2]);
+
+console.log(encodedABI);
+
+
+
+
+//WMATIC address= 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889
+//UniswapV2= 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff
+//AA=0xcf3C09Ae6124Ddc24e1970314308ee6869Ab39f2
+
+
+
